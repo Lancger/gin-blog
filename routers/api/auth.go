2d67470aba50de72313834b0e8e31ccd34bd1e64
xumanbai <xumanbai@onething.net>
2019-05-17 19:44:26 +0800

    修改登录逻辑为POST

--- routers/api/auth.go
+++ routers/api/auth.go
@@ -4,12 +4,12 @@ import (
 	"gin-blog/pkg/logging"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
 
+	"gin-blog/models"
 	"gin-blog/pkg/e"
 	"gin-blog/pkg/util"
-	"gin-blog/models"
 )
 
 type auth struct {
@@ -18,24 +18,41 @@ type auth struct {
 }
 
 func GetAuth(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
+
+	type reqPost struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	var (
+		req reqPost
+	)
+	// username := c.Query("username")
+	// password := c.Query("password")
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    e.ERROR,
+			"message": e.INVALID_PARAMS,
+		})
+		return
+	}
 
 	valid := validation.Validation{}
-	a := auth{Username: username, Password: password}
+	a := auth{Username: req.Username, Password: req.Password}
 	ok, _ := valid.Valid(&a)
 
 	data := make(map[string]interface{})
 	code := e.INVALID_PARAMS
 	if ok {
-		isExist := models.CheckAuth(username, password)
+		isExist := models.CheckAuth(req.Username, req.Password)
 		if isExist {
-			token, err := util.GenerateToken(username, password)
+			token, err := util.GenerateToken(req.Username, req.Password)
 			if err != nil {
 				code = e.ERROR_AUTH_TOKEN
 			} else {
 				data["token"] = token
-				
+
 				code = e.SUCCESS
 			}
 
@@ -44,13 +61,13 @@ func GetAuth(c *gin.Context) {
 		}
 	} else {
 		for _, err := range valid.Errors {
-            logging.Info(err.Key, err.Message)
-        }
+			logging.Info(err.Key, err.Message)
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-        "code" : code,
-        "msg" : e.GetMsg(code),
-        "data" : data,
-    })
+		"code": e.SUCCESS,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
--- routers/routers.go
+++ routers/routers.go
@@ -1,55 +1,68 @@
 package routers
 
 import (
-    "github.com/gin-gonic/gin"
+	"time"
 
-    "github.com/swaggo/gin-swagger"
-    "github.com/swaggo/gin-swagger/swaggerFiles"
-    _ "gin-blog/docs"      
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 
-    "gin-blog/routers/api/v1"
-    "gin-blog/routers/api"
-    "gin-blog/pkg/setting"
-    "gin-blog/middleware/jwt"
+	_ "gin-blog/docs"
+
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	"gin-blog/middleware/jwt"
+	"gin-blog/pkg/setting"
+	"gin-blog/routers/api"
+	v1 "gin-blog/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
-    r := gin.New()
+	r := gin.New()
 
-    r.Use(gin.Logger())
+	// 允许使用跨域请求  全局中间件
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc:  func(origin string) bool { return true },
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "X-Token"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-    r.Use(gin.Recovery())
+	r.Use(gin.Logger())
 
-    gin.SetMode(setting.RunMode)
+	r.Use(gin.Recovery())
 
-    r.POST("/auth", api.GetAuth)
+	gin.SetMode(setting.RunMode)
 
-    //swagger api docs
-    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.POST("/auth", api.GetAuth)
 
-    apiv1 := r.Group("/api/v1")
-    apiv1.Use(jwt.JWT())
-    {
-        //获取标签列表
-        apiv1.GET("/tags", v1.GetTags)
-        //新建标签
-        apiv1.POST("/tags", v1.AddTag)
-        //更新指定标签
-        apiv1.PUT("/tags/:id", v1.EditTag)
-        //删除指定标签
-        apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	//swagger api docs
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-        //获取文章列表
-        apiv1.GET("/articles", v1.GetArticles)
-        //获取指定文章
-        apiv1.GET("/articles/:id", v1.GetArticle)
-        //新建文章
-        apiv1.POST("/articles", v1.AddArticle)
-        //更新指定文章
-        apiv1.PUT("/articles/:id", v1.EditArticle)
-        //删除指定文章
-        apiv1.DELETE("/articles/:id", v1.DeleteArticle)
-    }
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
 
-    return r
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+	}
+
+	return r
 }
